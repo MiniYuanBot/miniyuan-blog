efbcd251482ab49b0849148d6323be462205cba1
MiniYuanBot <2175832500@qq.cn>
2026-02-11 14:36:32 +0800

    初步完成了pages/index.astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -11,6 +11,7 @@ const blog = defineCollection({
 		z.object({
 			title: z.string(),
 			description: z.string(),
+			author: z.string().optional(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
